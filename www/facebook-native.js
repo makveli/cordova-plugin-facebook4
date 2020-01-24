f1301d6033b6136b04be5739e2df93ab87a50730
makveli <pedrohenriqueissa@gmail.com>
2020-01-24 06:42:33 -0300

    Update facebook-native.js

--- www/facebook-native.js
+++ www/facebook-native.js
@@ -1,9 +1,5 @@
 var exec = require('cordova/exec')
 
-exports.getLoginStatus = function getLoginStatus (s, f) {
-  exec(s, f, 'FacebookConnectPlugin', 'getLoginStatus', [])
-}
-
 exports.showDialog = function showDialog (options, s, f) {
   exec(s, f, 'FacebookConnectPlugin', 'showDialog', [options])
 }
