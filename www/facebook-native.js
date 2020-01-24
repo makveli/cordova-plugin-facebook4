e4b8d9b98d8b2785f9a7d7a3aea34ae33ce56777
makveli <pedrohenriqueissa@gmail.com>
2020-01-24 07:14:10 -0300

    Update facebook-native.js

--- www/facebook-native.js
+++ www/facebook-native.js
@@ -1,9 +1,5 @@
 var exec = require('cordova/exec')
 
-exports.showDialog = function showDialog (options, s, f) {
-  exec(s, f, 'FacebookConnectPlugin', 'showDialog', [options])
-}
-
 exports.login = function login (permissions, s, f) {
   exec(s, f, 'FacebookConnectPlugin', 'login', permissions)
 }
